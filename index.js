d210bc52d6b09f0a091835237a3c151ceeb2fe20
olla <74874659+olla@users.noreply.github.com>
2021-01-20 13:16:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,30 +12,57 @@ function doclick(e) {
 	document.getElementById(Id1).parentNode.style.color = "var(--" + Id1 + "-color)";
 	document.getElementById(Id2).parentNode.style.color = "var(--" + Id2 + "-color)";
 	document.getElementById(Id3).parentNode.style.color = "var(--" + Id3 + "-color)";
+	document.getElementById(Id1).style.backgroundColor = "var(--DOM-color)";
+	document.getElementById(Id2).style.backgroundColor = "var(--DOM-color)";
+	document.getElementById(Id3).style.backgroundColor = "var(--DOM-color)";
+	document.getElementById(Id1).style.color = "var(--" + Id1 + "-color)";
+	document.getElementById(Id2).style.color = "var(--" + Id2 + "-color)";
+	document.getElementById(Id3).style.color = "var(--" + Id3 + "-color)";
 	e.target.parentNode.style.color = "var(--DOM-color)";
 }
+
 function dokeyup(e) {
 	"use strict";
+	/* eslint-disable no-console */
 	console.log(e.target.id + "--[dwn]--" + e.target.value);
-	// eslint-disable-line no-console
+	/* eslint-enable no-console */
 	e.target.style.width = e.target.value.length * MAT + "ex";
-	if (e.target.value.length < 2) { e.target.style.width = "1ex"; }
-	// background
-	//document.body.style.backgroundColor = "var(--DOM-color)";
+	if (e.target.value.length < 2) {
+		e.target.style.width = "1ex";
+	}
 }
+
+function dodatalistoption(e) {
+	"use strict";
+	/* eslint-disable no-console */
+	console.log(e.target.id + "--[opt]--" + e.target.value);
+	/* eslint-enable no-console */
+	e.target.style.width = e.target.value.length * MAT + "ex";
+	if (e.target.value.length < 2) {
+		e.target.style.width = "1ex";
+	}
+}
+
 function dokeydown(e) {
 	"use strict";
+	/* eslint-disable no-console */
 	console.log(e.target.id + "--[kup]--" + e.target.value);
-	// eslint-disable-line no-console
+	/* eslint-enable no-console */
 	e.target.style.width = e.target.value.length * MAT + "ex";
-	if (e.target.value.length < 2) { e.target.style.width = "1ex"; }
+	if (e.target.value.length < 2) {
+		e.target.style.width = "1ex";
+	}
 	// background
 	document.body.style.backgroundColor = "var(--" + e.target.id + "-color)";
+	e.target.style.backgroundColor = "var(--" + e.target.id + "-color)";
+	e.target.style.color = "var(--DOM-color)";
 }
+
 function theDomisloaded() {
 	"use strict";
- console.info('DOM');
-	// eslint-disable-line no-console
+	/* eslint-disable no-console */
+	console.info('DOM');
+	/* eslint-enable no-console */
 }
 
 document.addEventListener('DOMContentLoaded', theDomisloaded);
@@ -45,6 +72,9 @@ document.getElementById(Id3).value = "";
 document.getElementById(Id1).addEventListener('click', doclick);
 document.getElementById(Id2).addEventListener('click', doclick);
 document.getElementById(Id3).addEventListener('click', doclick);
+document.getElementById(Id1).addEventListener('input', dodatalistoption);
+document.getElementById(Id2).addEventListener('input', dodatalistoption);
+document.getElementById(Id3).addEventListener('input', dodatalistoption);
 document.getElementById(Id1).addEventListener('keydown', dokeydown);
 document.getElementById(Id2).addEventListener('keydown', dokeydown);
 document.getElementById(Id3).addEventListener('keydown', dokeydown);
